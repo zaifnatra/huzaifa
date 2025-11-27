4caccc39fa1f196ce892396a449f5ea9991fec47
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-11-27 17:51:37 -0500

    tut from yt for basic
    - importing forms module
    - functiosn
    - models
    - learning tailwind

--- myApp/src/app/app.ts
+++ myApp/src/app/app.ts
@@ -1,12 +1,40 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import for forms
+import { FormsModule } from '@angular/forms'
+
+// defining interface for taks
+interface Todo {
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // adding forms to the imports
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('myApp');
+  // variable for the text input
+  newTask = '';
+
+  // Arrau to hold the lists of tasks
+  todos: Todo[] = [
+    { task : 'Learn Angular', completed: false},
+    { task: 'Play League with Gur', completed: false},
+    { task: 'Learn TS', completed: false}
+  ];
+
+// function that runs when I click "add" button
+  addTodo() {
+    if (this.newTask.trim() !== '') {
+      this.todos.push({
+        task: this.newTask,
+        completed: false
+      });
+      this.newTask = '';
+    }
+  }
 }
