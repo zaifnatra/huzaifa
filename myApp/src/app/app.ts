2b97e2dd258970992dc7c3e8fc39af155f44c830
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-12-09 13:52:46 -0500

    Template
    - Prompt Gemini 3 to create a template for a personal website
    - Project Section: display hackathon & personal
    - Life: Show achievements I am proud of, life style goals, etc

--- myApp/src/app/app.ts
+++ myApp/src/app/app.ts
@@ -1,40 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import for forms
-import { FormsModule } from '@angular/forms'
-
-// defining interface for taks
-interface Todo {
-  task: string;
-  completed: boolean;
-}
 
 @Component({
   selector: 'app-root',
-  // adding forms to the imports
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  // variable for the text input
-  newTask = '';
-
-  // Arrau to hold the lists of tasks
-  todos: Todo[] = [
-    { task : 'Learn Angular', completed: false},
-    { task: 'Play League with Gur', completed: false},
-    { task: 'Learn TS', completed: false}
-  ];
-
-// function that runs when I click "add" button
-  addTodo() {
-    if (this.newTask.trim() !== '') {
-      this.todos.push({
-        task: this.newTask,
-        completed: false
-      });
-      this.newTask = '';
-    }
-  }
+  // Logic can be added here later if needed
 }
